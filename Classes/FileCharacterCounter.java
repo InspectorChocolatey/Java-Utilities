6f1e23d483b0c6b352073fe2480474453204e356
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2018-02-19 16:47:54 -0600

    Update FileCharacterCounter.java

--- Classes/FileCharacterCounter.java
+++ Classes/FileCharacterCounter.java
@@ -10,17 +10,13 @@ public class FileCharacterCounter
     public static void main(String[]args) throws FileNotFoundException        
     { 
     	Scanner console = new Scanner(System.in);           
-
     	System.out.println("File to be read: ");
         String inputFile = console.next();
-
         File file = new File(inputFile);
         Scanner in = new Scanner(file);
-
         int words = 0;
         int lines = 0;
         int chars = 0;
-
         while(in.hasNextLine())  
 		{
 			lines++;
@@ -28,9 +24,8 @@ public class FileCharacterCounter
         	chars += line.length();
         	words += new StringTokenizer(line, " ,").countTokens();
     	}
-
     	System.out.println("Number of lines: " + lines);
     	System.out.println("Number of words: " + words);
-		System.out.println("Number of characters: " + chars);
+	System.out.println("Number of characters: " + chars);
     }
 }
