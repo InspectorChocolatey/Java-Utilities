5a328d321befb419abda90b802138118ac9302b6
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2018-02-22 16:47:24 -0600

    Update Hostname.java

--- Classes/Hostname.java
+++ Classes/Hostname.java
@@ -1,7 +1,19 @@
 import java.lang.*;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
-public static String hostname(){ String hostname = "Unknown";
- try {      InetAddress addr;      addr = InetAddress.getLocalHost();      hostname = addr.getHostName(); } catch (UnknownHostException ex) {      hostname = "unknown"; } return hostname;}
-String obj = hostname();
-if(obj != "GP*"){ System.out.println("This does not appear to be a general parts computer!");}
+
+	public static String hostname()
+	{
+		String hostname;
+		try 
+		{
+			java.net.InetAddress address;
+			address = java.net.InetAddress.getLocalHost();
+			hostname = address.getHostName();
+		}
+		catch (UnknownHostException ex)
+		{
+			hostname = "Unknown";
+		}
+		return hostname;
+	}
