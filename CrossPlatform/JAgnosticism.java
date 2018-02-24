51ef3c011cb213cc755c78cbb4dd7ca79122fecb
Nicholas Jacquet <nicholasjacquet2@gmail.com>
2018-02-24 17:49:10 -0600

    Update JAgnosticism.java

--- CrossPlatform/JAgnosticism.java
+++ CrossPlatform/JAgnosticism.java
@@ -1,11 +1,42 @@
+
 public class JAgnosticism 
 {
-	public static String operatingsystemName() 
+	public static String operatingSystemName() 
 	{
 		return System.getProperty("os.name");
 	}
 	
-	public static Boolean environmentIs64Bit() 
+	
+	public static String computername() throws java.net.UnknownHostException 
+	{
+	    	java.net.InetAddress localMachine = java.net.InetAddress.getLocalHost();
+	    	return localMachine.getHostName();
+	}
+	
+	
+	
+//	public static String computername()
+//	{
+//		String hostname;
+//		try 
+//		{
+//			java.net.InetAddress address;
+//			address = java.net.InetAddress.getLocalHost();
+//			hostname = address.getHostName();
+//		}
+//		catch (java.net.UnknownHostException ex)
+//		{
+//			hostname = "Unknown";
+//		}
+//		return hostname;
+//	}
+	
+	public static String username() 
+	{
+	    	return System.getProperty("user.name");
+	}
+	
+	public static Boolean operatingSystemIs64Bit() 
 	{
 		boolean is64bit = false;
 		if (System.getProperty("os.name").contains("Windows")) 
@@ -18,4 +49,6 @@ public class JAgnosticism
 		}
 		return is64bit;
 	}
+	
+	
 }
